943250f575205cf73bf56fc1d14f0f4bfeaa0776
nikhilrana884 <nikhilrana884@gmail.com>
2022-01-22 23:39:07 +0530

    Header Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Header from '.components/Header'
+import Footer from '.components/Footer'
 
 const App = () => {
   return (
+    <>
+    
+  <Header />
+  <Footer />
     <div className="App">
       <h1> Remote Health Monitoring
         {/* No coding today bcoz of tomorrows's exam. */} 
       </h1>
     </div>
+    </>
   );
 }
 
